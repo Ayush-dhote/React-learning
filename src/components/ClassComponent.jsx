06d416f1b4b2cfa7b0b5d868223b92f11bc5bc39
Ayush-dhote <ayushdhote2001@gmail.com>
2025-07-25 09:19:24 +0530

    "initial commit"

---
+++ src/components/ClassComponent.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+
+  constructor(props) {
+    super(props);  
+    this.state = {
+      count: 0,
+    };   
+  }
+
+
+  componentDidMount() { 
+    console.log('Component mounted');
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component updated');
+    console.log('Previous state:', prevState);
+    console.log('Current state:', this.state);
+  }
+
+  componentWillUnmount() {
+    console.log('Component will unmount');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+      </div>
+    )
+  }
+}
